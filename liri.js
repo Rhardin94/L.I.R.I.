9965c60767f3d3033ca3681dded479108b36fab3
Robert H <robertjameshardin@aol.com>
2019-03-31 14:31:21 -0500

    outputs mr nobody if movie search left blank

--- liri.js
+++ liri.js
@@ -7,6 +7,9 @@ const spotify = new Spotify(keys.spotify);
 //Capturing console input to determine which part of app to run
 let apiType = process.argv[2];
 let Search = process.argv.slice(3).join(" ");
+if (Search === "") {
+    Search = "mr nobody";
+}
 //Switch used to determine which API to interact with
 switch (apiType) {
     case "spotify-this-song":
@@ -52,7 +55,7 @@ console.log("Title: " + response.data.Title);
 console.log("Release Year: " + response.data.Released);
 console.log("IMDB Rating: " + response.data.Ratings[0].Value);
 console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-console.log("Country/ies Produced in: " + response.data.Country);
+console.log("Country/s Produced In: " + response.data.Country);
 console.log("Language: " + response.data.Language);
 console.log("Plot: " + response.data.Plot);
 console.log("Actors: " + response.data.Actors);
